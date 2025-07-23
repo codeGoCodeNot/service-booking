f42388b7f9b1477040a69c82163d82bf08ada002
sendosend <johnsenberdin2930@gmail.com>
2025-07-24 07:21:40 +0800

    Added some functionality

--- helper.js
+++ helper.js
@@ -6,3 +6,37 @@ export const themeToggle = () => {
   document.querySelector(".theme-toggle span").textContent =
     newTheme === "dark" ? "Light Theme" : "Dark Theme";
 };
+
+export const popUp = () => {
+  const bookBtns = document.querySelectorAll(".bookbtn");
+  const content = document.querySelectorAll(".content-container");
+
+  bookBtns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      const popup = document.createElement("div");
+      popup.classList.add("popup");
+      popup.innerHTML = `
+      <h3 class="popup-title">Book Now!</h3>
+        <p><strong>No cancellations</strong> once booked — secure your spot confidently.</p>
+        <p>Enjoy <strong>easy installment plans</strong> for your convenience!</p>
+        <p class="innerPara"><strong>⚡ Get 10% OFF</strong> for early reservations. Don’t miss out!</p>
+        `;
+
+      const closeBtn = document.createElement("button");
+      closeBtn.classList.add("close-btn");
+      closeBtn.textContent = "x";
+      closeBtn.addEventListener("click", () => {
+        popup.remove();
+      });
+
+      const payBtn = document.createElement("button");
+      payBtn.classList.add("pay-btn");
+      payBtn.textContent = "Pay here";
+      payBtn.style.width = "120px";
+
+      popup.append(payBtn);
+      popup.append(closeBtn);
+      content[index].append(popup);
+    });
+  });
+};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { themeToggle } from "./helper.js";
+import { popUp } from "./helper.js";
 
 document.querySelector(".theme-toggle").addEventListener("click", themeToggle);
+popUp();
