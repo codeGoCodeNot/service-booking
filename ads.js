ffdea26c31cde2f9b2d1925c9a268d6156e69a34
sendosend <johnsenberdin2930@gmail.com>
2025-07-24 19:15:46 +0800

    Ads added random

---
+++ ads.js
@@ -0,0 +1,49 @@
+const adImg = document.querySelector(".ad-img");
+const adsPopup = document.querySelector(".ads-popup");
+const ads = [
+  {
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/1PUBHERSHEYSmoresMcFlurry:1-column-desktop?resmode=sharp2",
+    link: "https://spankbank.com/",
+  },
+  {
+    img: "https://www.shutterstock.com/image-photo/bangi-malaysia-november-16-2019-600nw-1565709685.jpg",
+    link: "https://www.pornhub.com/",
+  },
+  {
+    img: "https://c0.wallpaperflare.com/preview/28/600/957/pair-of-black-white-and-red-air-jordan-1-shoes.jpg",
+    link: "https://www.bestjavporn.com/",
+  },
+  {
+    img: "https://c1.wallpaperflare.com/preview/659/106/422/casino-arcade-slot-machines-machines-gambling-risk.jpg",
+    link: "https://www.casinoplus.com.ph/",
+  },
+  {
+    img: "https://s7d1.scene7.com/is/image/mcdonalds/SnackWrap_COM_1Pub:1-column-desktop?resmode=sharp2",
+    link: "https://www.mcdonalds.com/us/en-us.html",
+  },
+];
+
+export const showAd = () => {
+  const randomIndex = Math.floor(Math.random() * ads.length);
+  const selectAd = ads[randomIndex];
+  adImg.src = selectAd.img;
+  adImg.onclick = () => window.open(selectAd.link, "_blank");
+
+  adsPopup.style.display = "block";
+};
+
+export const closeAds = () => {
+  const closeBtn = document.querySelector(".close-ad");
+
+  closeBtn.addEventListener("click", (event) => {
+    const chance = Math.floor(Math.random() * 3);
+    if (chance <= 1) {
+      const randomIndex = Math.floor(Math.random() * ads.length);
+      window.open(ads[randomIndex].link, "_blank");
+    } else {
+      adsPopup.style.display = "none";
+    }
+
+    event.preventDefault();
+  });
+};
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { themeToggle } from "./helper.js";
 import { popUp } from "./helper.js";
+import { showAd, closeAds } from "./ads.js";
 
 document.querySelector(".theme-toggle").addEventListener("click", themeToggle);
 popUp();
+closeAds();
+showAd();
